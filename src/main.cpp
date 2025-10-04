5423be89e3c1744f7ea39db53501f84da2084a75
Adit0507 <adi05072002@gmail.com>
2025-10-05 00:50:12 +0530

    shader(dy) stuff :alien:

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <iostream>
+#include <cmath>
 
 const char *vertextShaderSource = "#version 330 core\n"
                                   "layout (location = 0) in vec3 aPos;\n"
                                   "void main()\n"
                                   "{\n"
-                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                  "   gl_Position = vec4(aPos, 1.0);\n"
                                   "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
+                                   "uniform vec4 ourColor; \n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "   FragColor = ourColor;\n"
                                    "}\n\0";
 
 void processInput(GLFWwindow *window)
@@ -117,8 +119,13 @@ int main()
         glClearColor(1.0f, 0.8f, 0.5f, 2.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // drawing the triangle
         glUseProgram(shaderProgram);
+        double timeValue = glfwGetTime();
+        float greenValue = static_cast<float> (sin(timeValue) / 2.0 + 0.5);
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        
+        // drawing the triangle
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 5);
 
